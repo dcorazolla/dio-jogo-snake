e80bb39893fc29993ad5cf44a0c92bbe546a05c8
Diogo Corazolla <diogocorazolla@gmail.com>
2020-10-30 15:46:42 -0300

    movimentos por w,a,s,d tambem

--- js/main.js
+++ js/main.js
@@ -140,19 +140,19 @@
      */
     let getKey = (event) => {
         if (movimento == false) {
-            if (event.keyCode == 37 && defs.direction != "right") {
+            if ((event.keyCode == 37 || event.keyCode == 65) && defs.direction != "right") {
                 defs.direction = "left";
                 movimento = true;
             }
-            else if (event.keyCode == 38 && defs.direction != "down") {
+            else if ((event.keyCode == 38 || event.keyCode == 87) && defs.direction != "down") {
                 defs.direction = "up";
                 movimento = true;
             }
-            else if (event.keyCode == 39 && defs.direction != "left") {
+            else if ((event.keyCode == 39 || event.keyCode == 68) && defs.direction != "left") {
                 defs.direction = "right";
                 movimento = true;
             }
-            else if (event.keyCode == 40 && defs.direction != "up") {
+            else if ((event.keyCode == 40 || event.keyCode == 83) && defs.direction != "up") {
                 defs.direction = "down";
                 movimento = true;
             }
